b5fddcd0be440baf9fce4b22dd8db49aeee564af
inakiUnzaga <inakiunzaga88@gmail.com>
2024-09-22 16:21:08 -0300

    Problemas con SSL bcra ( investigando )

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import requests
 
+Numero="2024-09-20"
+fecha= f"fecha={Numero}"
+
+urlMaestro= "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Maestros/Divisas"
+urlCotizaciones= f"https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?{fecha}"
+
+
+data = requests.get(urlCotizaciones,verify=False).json()
+
